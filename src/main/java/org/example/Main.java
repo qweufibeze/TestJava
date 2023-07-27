abc56a2c50ec9585cd237b5ba2e84caf844722cc
alexandrobol <qweufibeze@gmail.com>
2023-07-27 13:16:49 +0300

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,30 @@
 package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Установите путь к драйверу для вашего браузера (ChromeDriver, GeckoDriver и т. д.)
+        System.setProperty("webdriver.chrome.driver", "путь_к_драйверу/chromedriver.exe");
+
+        // Создание экземпляра WebDriver для работы с Google Chrome
+        WebDriver driver = new ChromeDriver();
+
+        // Открытие страницы в браузере
+        driver.get("https://www.example.com"); // Замените на нужный URL
+
+        // Получение заголовка страницы
+        String pageTitle = driver.getTitle();
+
+        // Проверка наличия текста в заголовке страницы
+        if (pageTitle.contains("Example Domain")) {
+            System.out.println("Тест пройден: заголовок страницы содержит 'Example Domain'.");
+        } else {
+            System.out.println("Тест не пройден: заголовок страницы не содержит 'Example Domain'.");
+        }
+
+        // Закрытие браузера и завершение работы WebDriver
+        driver.quit();
     }
 }
