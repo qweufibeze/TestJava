b183e31eada61d5b5ba2f1938852c68b581b1e9e
alexandrobol <qweufibeze@gmail.com>
2023-07-29 17:34:44 +0300

    пофиксил проверку авторизации

--- src/main/java/org/example/UserAuthorizationTest.java
+++ src/main/java/org/example/UserAuthorizationTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import net.bytebuddy.dynamic.loading.ByteArrayClassLoader;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,41 +15,36 @@ import java.util.List;
 import static org.junit.Assert.assertTrue;
 
 public class UserAuthorizationTest extends MainMethod{
+    static final MainMethod mainMethod = new MainMethod();
     static final WebDriver driverChrome = new ChromeDriver();
     static final String BASE_URL = "https://my.tretyakov.ru/app/";
-
+    @Test
     public static void main(String[] args) {
         authorizationWayIcon();
+        getBackToMainMenu();
+        authorizationWayWord();
+        getBackToMainMenu();
     }
+    @Test
     public static void authorizationWayIcon(){
-        MainMethod mainMethod = new MainMethod();
         mainMethod.setUp(driverChrome,BASE_URL);
-
         //Нажимаем на иконку авторизации
         WebElement authorizationIcon = driverChrome.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/header/div/div/div[2]/a"));
         if(authorizationIcon.isDisplayed()) {
             authorizationIcon.click();
         }
-
         enterLogPass();
-
-        mainMethod.setUp(driverChrome,driverChrome.getCurrentUrl());
-        WebElement secondIconButton = driverChrome.findElement(By.xpath("/html/body/div/div/div/div[2]/header/div/div/div[2]/a[1]"));
-        if(secondIconButton.isDisplayed()) {
-            secondIconButton.click();
-        }
-
-        WebDriverWait wait = new WebDriverWait(driverChrome,Duration.ofMillis(500L));
-        WebElement exitButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div/div/div/div[2]/div/div/nav/ul/li[4]/a")));
-        exitButton.click();
-
+    }
+    @Test
+    public static void authorizationWayWord(){
+        mainMethod.setUp(driverChrome,BASE_URL);
         WebElement authorizationWord = driverChrome.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/header/div/div/div[2]/div[1]"));
         if(authorizationWord.isDisplayed()) {
             authorizationWord.click();
         }
         enterLogPass();
     }
-
+    @Test
     public static void enterLogPass(){
         WebElement loginInput = driverChrome.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div/div/div/div/div[2]/form/div[1]/label"));
         if(loginInput.isDisplayed()) {
@@ -66,6 +62,19 @@ public class UserAuthorizationTest extends MainMethod{
         if(enterButton.isDisplayed()) {
             enterButton.click();
         }
+    }
+    @Test
+    public static void getBackToMainMenu(){
+        //Возвращение к MainMenu
+        mainMethod.setUp(driverChrome,driverChrome.getCurrentUrl());
+        WebElement secondIconButton = driverChrome.findElement(By.xpath("/html/body/div/div/div/div[2]/header/div/div/div[2]/a[1]"));
+        if(secondIconButton.isDisplayed()) {
+            secondIconButton.click();
+        }
+
+        WebDriverWait wait = new WebDriverWait(driverChrome,Duration.ofMillis(500L));
+        WebElement exitButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div/div/div/div[2]/div/div/nav/ul/li[4]/a")));
+        exitButton.click();
     }
 }
 
