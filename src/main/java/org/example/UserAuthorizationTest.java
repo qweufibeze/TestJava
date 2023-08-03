af7963bb4d0642d6da994410da161fc666f69c38
alexandrobol <qweufibeze@gmail.com>
2023-08-03 20:45:08 +0300

    убрал лишние дефствия, добавил authorizationTest() по урлу на который мы проваливаемся после авторизации

--- src/main/java/org/example/UserAuthorizationTest.java
+++ src/main/java/org/example/UserAuthorizationTest.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -17,7 +19,7 @@ public class UserAuthorizationTest extends MainMethod{
      final static WebDriver driverChromeIcon = new ChromeDriver();
      final static WebDriver driverChromeWord = new ChromeDriver();
      final static String BASE_URL = "https://my.tretyakov.ru/app/";
-     final static String AUTH_URL = "https://my.tretyakov.ru/api/auth/";
+     final static String AUTH_URL = "https://my.tretyakov.ru/app/profile/detail";
 
     // Проверяем авторизацию по иконке и по слову на сайте "https://my.tretyakov.ru/app/" параллельно друг другу
 
@@ -25,37 +27,6 @@ public class UserAuthorizationTest extends MainMethod{
         runTestAuthorization();
     }
 
-//        "status": false,
-//            "error": {
-//        "code": "FILL_REQUIRED_FIELDS",
-//                "message": "Заполните обязательные поля."
-//    }
-//    @Test
-//    public void testSuccessfulAuthorization() {
-//        RestAssured.baseURI = "https://my.tretyakov.ru";
-//
-//        String email = "qweufibeze@gmail.com";
-//        String password = "65ufibezedeZ+-";
-//
-//        String requestBody = "{ \"email\": \"" + email + "\", \"password\": \"" + password + "\" }";
-//
-//        given()
-//                .contentType(ContentType.JSON)
-//                .header(":authority", "https://my.tretyakov.ru")
-//                .header(":method", "POST")
-//                .header(":path", "/api/auth/")
-//                .header(":scheme", "https")
-//                .header("Accept", "application/json, text/plain, */*")
-//                .header("Accept-Encoding", "gzip, deflate, br")
-//                .header("Accept-Language", "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7")
-//                .header("Content-Type", "application/x-www-form-urlencoded")
-//                .body(requestBody)
-//                .when()
-//                .post("https://my.tretyakov.ru/api/auth/")
-//                .then().log().all()
-//                .statusCode(200)
-//                .body("status", equalTo("success"));
-//    }
     public static void runTestAuthorization() {
          Runnable iconAuthorizationTask = () -> authorizationWayIcon();
          Runnable wordAuthorizationTask = () -> authorizationWayWord();
@@ -65,7 +36,6 @@ public class UserAuthorizationTest extends MainMethod{
          iconThread.start();
          Thread wordThread = new Thread(wordAuthorizationTask);
          wordThread.start();
-
      }
     public static void authorizationWayIcon(){
         mainMethod.setUp(driverChromeIcon,BASE_URL);
@@ -75,16 +45,6 @@ public class UserAuthorizationTest extends MainMethod{
             authorizationIcon.click();
         }
         enterLogPass(driverChromeIcon);
-
-        mainMethod.setUp(driverChromeIcon,BASE_URL);
-        WebElement secondIconButton = driverChromeIcon.findElement(By.xpath("/html/body/div/div/div/div[2]/header/div/div/div[2]/a[1]"));
-        if(secondIconButton.isDisplayed()) {
-            secondIconButton.click();
-        }
-        WebDriverWait wait = new WebDriverWait(driverChromeIcon,Duration.ofMillis(3000L));
-        WebElement exitButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div/div/div/div[2]/div/div/nav/ul/li[4]/a")));
-        exitButton.click();
-        mainMethod.tearDown(driverChromeIcon, driverChromeIcon.getCurrentUrl());
     }
     public static void authorizationWayWord(){
         mainMethod.setUp(driverChromeWord,BASE_URL);
@@ -93,16 +53,6 @@ public class UserAuthorizationTest extends MainMethod{
             authorizationWord.click();
         }
         enterLogPass(driverChromeWord);
-
-        mainMethod.setUp(driverChromeWord,BASE_URL);
-        WebElement secondIconButton = driverChromeWord.findElement(By.xpath("/html/body/div/div/div/div[2]/header/div/div/div[2]/a[1]"));
-        if(secondIconButton.isDisplayed()) {
-            secondIconButton.click();
-        }
-        WebDriverWait wait = new WebDriverWait(driverChromeWord,Duration.ofMillis(3000L));
-        WebElement exitButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div/div/div/div[2]/div/div/nav/ul/li[4]/a")));
-        exitButton.click();
-        mainMethod.tearDown(driverChromeWord, driverChromeWord.getCurrentUrl());
     }
     public static void enterLogPass(WebDriver webDriver){
         WebElement loginInput = webDriver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div/div/div/div/div[2]/form/div[1]/label"));
@@ -121,12 +71,12 @@ public class UserAuthorizationTest extends MainMethod{
         if(enterButton.isDisplayed()) {
             enterButton.click();
         }
-        WebDriverWait wait = new WebDriverWait(webDriver,Duration.ofMillis(3000L));
-        WebElement errorLogPass = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div/div[2]/div/div/div/div/div[2]/form/span")));
-        if (errorLogPass.isDisplayed()) {
-            System.out.println("test not ok");
-        } else {
-            System.out.println("test ok");
+    }
+    @Test
+    public void authorizationTest(){
+        runTestAuthorization();
+        if(driverChromeIcon.getCurrentUrl().contains(AUTH_URL) && driverChromeWord.getCurrentUrl().contains(AUTH_URL)){
+            Assert.assertTrue(true);
         }
     }
 }
