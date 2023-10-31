fa2715472ddd2bc7afe71a051a93c8d20caefa21
alexandrobol <qweufibeze@gmail.com>
2023-10-31 17:24:51 +0300

    Добавил проверку CSS стиля у случайного параметра на странице после авторизации

--- src/test/java/org/example/CheckCSSStyleTest.java
+++ src/test/java/org/example/CheckCSSStyleTest.java
@@ -5,14 +5,13 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import static org.junit.Assert.*;
-import static org.example.CheckCSSStyle.*;
 
-public class CheckCSSStyleTest  {
+
+public class CheckCSSStyleTest extends CheckCSSStyle  {
     @Category(AuthorizationTests.class)
     @Test
     public void accessGetParamCSS() {
         String param = getStyleCSS();
-
         if (param.contains("Montserrat, sans-serif")) {
             assertTrue(true);
         }else{
