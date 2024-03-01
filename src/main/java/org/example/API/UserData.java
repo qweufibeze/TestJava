43e67cf1ee214bed34726cf35e1bf00129eda787
a.balobanov <a.balobano@gardatech.ru>
2024-03-01 19:24:17 +0300

    final

--- src/main/java/org/example/API/UserData.java
+++
@@ -1,4 +0,0 @@
-package org.example.API;
-
-public class UserData {
-}
--- src/main/java/org/example/UserAuthorization.java
+++ src/main/java/org/example/UserAuthorization.java
@@ -1,11 +1,10 @@
 package org.example;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
 
 public class UserAuthorization extends MainMethod {
